5b1b79da97c2d2e5b3b9ec3e9691f61e4472e3f7
shubhambashte <shubhamrbashte@gmail.com>
2024-03-07 11:10:46 +0530

    commit

---
+++ Arithmeticoperator.js
@@ -0,0 +1,19 @@
+let a=6, b=5;
+let c=a+b;
+document.writeln("\n addition of a and b:" ,c); 
+
+ c=a-b;
+document.writeln(" subtract of a and b:" ,c);
+
+ c=a*b;
+document.write(" multiply of a and b:" ,c);
+
+ c=a/b;
+document.write(" divide of a and b:" ,c);
+
+ c=a**b;
+document.write(" expo of a and b:" ,c);
+
+c=a%b;
+document.write("Modulus of a and b:" ,c);
+
