7440496e39e1b67e5177b7b3771583b4111c009c
shubhambashte <shubhamrbashte@gmail.com>
2024-03-12 12:00:08 +0530

    For loop eg

---
+++ iteration.js
@@ -0,0 +1,5 @@
+//for iteration
+let example="Shubham";
+for(let i=1; i<=20; i++){
+    console.log( i + ":"+ example)
+}
---
+++ table.js
@@ -0,0 +1,5 @@
+//for table
+let num=7;
+for(let i=1; i<=10; i++){
+    console.log(num*i)
+}
