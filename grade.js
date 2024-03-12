92ef67b3a605ba60d34d7d63b410c4d4acf28dec
shubhambashte <shubhamrbashte@gmail.com>
2024-03-12 11:30:57 +0530

    grade

---
+++ grade.js
@@ -0,0 +1,17 @@
+let grade='B';
+let result;
+switch(grade){
+    case 'A':
+        result="A grade";
+        break;
+     case 'B':
+        result="B grade";
+        break; 
+      case 'C':
+        result="C grade";
+        break;
+     default:
+     result="No grade"
+        
+}
+console.log("Grade",result)
