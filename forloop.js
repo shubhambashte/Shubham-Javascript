e2769407a4d81f4b744171450e8fbec095669e30
shubhambashte <shubhamrbashte@gmail.com>
2024-03-16 11:36:01 +0530

    for loop

---
+++ forloop.js
@@ -0,0 +1,29 @@
+//print numbers from 1 to 60//
+for(let i=1;i<=60;i++){
+    console.log(i)
+}
+//print numbers from 60 to 0//
+for(let i=60;i>=0;i--){
+    console.log(i)
+}
+//print numbers from 0 to 60 in difference of 9//
+for(let i=0;i<=60;i=i+9){
+    console.log(i)
+}
+//print numbers from 12 to 24//
+for(let i=12;i<=24;i++){
+    console.log(i)
+}
+//print numbers from 6 to 31 with odd values only//
+for(let i=7;i<=31;i++){
+    if(i%2!=0){
+        console.log(i)
+    }
+}
+//print numbers from 10to -20//
+for(let i=10;i>=-20;i--){
+    if(i%2==0){
+        
+    }
+    console.log(i)
+}
