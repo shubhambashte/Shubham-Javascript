8326f958fc10a14f13ae0b883a82bb6daa37a179
shubhambashte <shubhamrbashte@gmail.com>
2024-03-18 12:01:03 +0530

    NEW LOOP

---
+++ newwhile.js
@@ -0,0 +1,16 @@
+let count = 100, sum = 0;
+while (count) { 
+  sum = sum + count;
+  count = count - 2; 
+}
+console.log(sum);
+
+
+
+
+let num = 1;
+while (num < 10) {
+    console.log(num);
+    num +=2;
+}
+Co
