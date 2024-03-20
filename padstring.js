7146d360b7a6b7371b80862e2095dc843385dbb9
shubhambashte <shubhamrbashte@gmail.com>
2024-03-20 18:25:36 +0530

    new strings

---
+++ padstring.js
@@ -0,0 +1,27 @@
+// let text = "5";
+// let padded = text.padStart(4,"0");
+// console.log(text)
+
+
+// let text = "5";
+// let padded = text.padStart(4,"x");
+// console.log(text)
+
+// let numb = 5;
+// let text = numb.toString();
+// let padded = text.padStart(4,"0");
+// console.log(numb)
+
+
+// let text = "5";
+// let padded = text.padEnd(4,"0");
+// console.log(text)
+
+// let text = "5";
+// let padded = text.padEnd(4,"x");
+// console.log(text)
+
+let numb = 5;
+let text = numb.toString();
+let padded = text.padEnd(4,"0");
+console.log(numb)
---
+++ repeat.js
@@ -0,0 +1,8 @@
+// let text = "Hello SHUBHAM!";
+// let result = text.repeat(2);
+// console.log(text)
+
+
+let text = "Hello Shubham!";
+let result = text.repeat(4);
+console.log(text)
