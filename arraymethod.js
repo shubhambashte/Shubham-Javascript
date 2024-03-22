6960292073e8d71967bb4c3af0b192eed22ede51
shubhambashte <shubhamrbashte@gmail.com>
2024-03-22 16:29:54 +0530

    array methods defined in comment

--- arraymethod.js
+++ arraymethod.js
@@ -1,50 +1,71 @@
-// const fruits =["Apple","Banana","Orange","Kiwi"];
+// const fruits =["Apple","Banana","Orange","Kiwi"];     
 // let size = fruits.length
-// console.log(size)
+// console.log(size)                                     //Array Length is defined
+
 
 // const fruits =["Apple","Banana","Orange","Kiwi"];
-// console.log(fruits.toString())
+// console.log(fruits.toString())                        //tostring()function in array method is defined to convert an array to a string of array values
 
-// const fruits =["Apple","Banana","Orange","Kiwi"];
+
+
+// const fruits =["Apple","Banana","Orange","Kiwi"];    // Array at() function is defined 
 // // let fruit = fruits.at(-5);
 //  fruit = fruits[1];
-// console.log(fruit)
+// console.log(fruit)  
 
-// const fruits =["Apple","Banana","Orange","Kiwi"];
+
+                                          
+// const fruits =["Apple","Banana","Orange","Kiwi"];     //Array join() function is defined to join all arrays elements into a string
 // console.log(fruits.join("+"))
+
+
 
 // const fruits =["Apple","Banana","Orange","Kiwi"];
 // fruits.pop()
-// console.log(fruits)
+// console.log(fruits)                                    //Array pop() function which removes the last element of array
+
+
 
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // fruits.push("Kiwi");
-// console.log(fruits)
+// console.log(fruits)                                     //Array push() function which adds a new element to an array at the end 
+
+
 
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // fruits.shift();
-// console.log(fruits)
+// console.log(fruits)                                      //Array shift() function removes the first array element and shifts all other elements to lower index
+
+
 
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // fruits.unshift();
-// console.log(fruits)
+// console.log(fruits)                                        //Array unshift() function adds a new element to an arra at the beginning and unshifts older elements
+
+
 
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // fruits[0] = "Kiwi";
-// console.log(fruits)
+// console.log(fruits)                                       //Array elements are accessed using their index number to identify the array element 
+
+
+
 
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // delete fruits[0];
-// console.log(fruits)
+// console.log(fruits)                                       //Array delete() function leaves undefined holes in array
 
 
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // fruits.copyWithin(2, 0, 2);
-// console.log(fruits)
+// console.log(fruits)                                        //Array copyWithin() function copies array elements to another position in array 
 
-const myArr = [[1,2],[3,4],[5,6]];
-const newArr = myArr.flat();
-console.log(newArr)
+
+
+
+// const myArr = [[1,2],[3,4],[5,6]];
+// const newArr = myArr.flat();
+// console.log(newArr)                                           //Array flat() method creates a new array with sub-array elements concatenated to a specified depth
 
 
 
